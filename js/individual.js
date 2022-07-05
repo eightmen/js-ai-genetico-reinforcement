b9ddb6c73f748e4ad216d7daf6845ffbd8352147
eightmen <118704527+eightmen@users.noreply.github.com>
2022-07-05 19:30:13 +0000

    KVM: nVMX: fix "acknowledge interrupt on exit" when APICv is in use

--- js/individual.js
+++ js/individual.js
@@ -16,3 +16,31 @@ function Individual(){
     this.state.w1 = [];
     for(var r = 0; r < numActions; r++){
       this.state.w1[r] = [];
+      for(var c = 0; c < numHidden; c++){
+        this.state.w1[r][c] = gaussianRand();
+      }
+    }
+  }
+
+  this.forwardPass = function(input){
+
+    // first matmul
+    var x = numeric.dot(this.state.w0, input);
+
+    // first activation
+    x = numeric.map(x, relu);
+
+    // second matmul
+    x = numeric.dot(this.state.w1, x);
+
+    // second activation (sigmoid)
+    x = numeric.map(x, sigmoid);
+    
+    return x;
+  }
+
+  this.getAction = function(input){
+    var actionValues = this.forwardPass(input, this.state.w0, this.state.w1);
+    return maxIndex(actionValues);
+  }
+}
---
+++ js/lib/numeric.js
@@ -0,0 +1,59 @@
+
+"use strict";
+
+var numeric = (typeof exports === "undefined")?(function numeric() {}):(exports);
+if(typeof global !== "undefined") { global.numeric = numeric; }
+
+numeric.version = "1.2.6";
+
+// 1. Utility functions
+numeric.bench = function bench (f,interval) {
+    var t1,t2,n,i;
+    if(typeof interval === "undefined") { interval = 15; }
+    n = 0.5;
+    t1 = new Date();
+    while(1) {
+        n*=2;
+        for(i=n;i>3;i-=4) { f(); f(); f(); f(); }
+        while(i>0) { f(); i--; }
+        t2 = new Date();
+        if(t2-t1 > interval) break;
+    }
+    for(i=n;i>3;i-=4) { f(); f(); f(); f(); }
+    while(i>0) { f(); i--; }
+    t2 = new Date();
+    return 1000*(3*n-1)/(t2-t1);
+}
+
+numeric._myIndexOf = (function _myIndexOf(w) {
+    var n = this.length,k;
+    for(k=0;k<n;++k) if(this[k]===w) return k;
+    return -1;
+});
+numeric.myIndexOf = (Array.prototype.indexOf)?Array.prototype.indexOf:numeric._myIndexOf;
+
+numeric.Function = Function;
+numeric.precision = 4;
+numeric.largeArray = 50;
+
+numeric.prettyPrint = function prettyPrint(x) {
+    function fmtnum(x) {
+        if(x === 0) { return '0'; }
+        if(isNaN(x)) { return 'NaN'; }
+        if(x<0) { return '-'+fmtnum(-x); }
+        if(isFinite(x)) {
+            var scale = Math.floor(Math.log(x) / Math.log(10));
+            var normalized = x / Math.pow(10,scale);
+            var basic = normalized.toPrecision(numeric.precision);
+            if(parseFloat(basic) === 10) { scale++; normalized = 1; basic = normalized.toPrecision(numeric.precision); }
+            return parseFloat(basic).toString()+'e'+scale.toString();
+        }
+        return 'Infinity';
+    }
+    var ret = [];
+    function foo(x) {
+        var k;
+        if(typeof x === "undefined") { ret.push(Array(numeric.precision+8).join(' ')); return false; }
+        if(typeof x === "string") { ret.push('"'+x+'"'); return false; }
+        if(typeof x === "boolean") { ret.push(x.toString()); return false; }
+        if(typeof x === "number") {
