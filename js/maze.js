1b019037e518aaa61c2064dea1bdda4d0c457231
eightmen <118704527+eightmen@users.noreply.github.com>
2023-03-21 19:07:55 +0000

    Add same website.

--- js/maze.js
+++ js/maze.js
@@ -64,4 +64,7 @@ function MazeGame(){
 
     // enemy
     vector[2] = (this.state.enemyPosition[0] - this.state.gridSize/2)/this.state.gridSize/2;
-    vector[3] = (this.state.enemyPosition[1] - this.state.gridSize/2)/this.state.grid
+    vector[3] = (this.state.enemyPosition[1] - this.state.gridSize/2)/this.state.gridSize/2;
+
+    // distance
+    // vector[4] = Math.abs(this.state.playerPosition[0] - this.stat
