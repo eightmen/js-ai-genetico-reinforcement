b0ce236d653a1acb7ce6b54f93f32485c0e2199d
eightmen <118704527+eightmen@users.noreply.github.com>
2023-05-05 19:42:09 +0000

    2.0.16

--- js/pong.js
+++ js/pong.js
@@ -3,4 +3,8 @@ function PongGame(){
   this.state.gridSize = 160;
   this.state.paddleSize = [4, 16];
   this.state.puckSize = [2, 4];
-  this.state.pad
+  this.state.paddleSpeed = 2;
+  this.state.baseBallVelocity = 2;
+  this.state.gameDuration = 21;
+
+  this.
